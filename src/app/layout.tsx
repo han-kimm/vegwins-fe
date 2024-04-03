b525f7e2a860a39eeaa7ebd65b45e6f32c3182c5
Haneul-Kim <4188989@naver.com>
2024-04-03 10:54:26 +0900

    setting: Tailwind CSS config 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,14 @@ import "./globals.css";
 
 const NotoSansKR = localFont({
   src: [
-    { path: "../../public/font/NotoSans_light_sub.woff2", weight: "300", style: "normal" },
+    { path: "../../public/font/NotoSans_regular_sub.woff2", weight: "400", style: "normal" },
     { path: "../../public/font/NotoSans_medium_sub.woff2", weight: "500", style: "normal" },
     { path: "../../public/font/NotoSans_bold_sub.woff2", weight: "700", style: "normal" },
   ],
 });
 
 export const metadata: Metadata = {
-  title: "함께 시작해요 - Vegwins",
+  title: "함께 시작해요, 비긴즈",
   description: "편의점, 마트에 있는 비건 제품을 찾아봅시다.",
 };
 
@@ -21,8 +21,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" className={NotoSansKR.className}>
-      <body>{children}</body>
+    <html lang="ko">
+      <body className={NotoSansKR.className}>{children}</body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,57 @@
 import type { Config } from "tailwindcss";
 
+const createPxEntries = (size: number) => {
+  return {
+    0: "0",
+    ...Array.from(Array(size + 1)).reduce((accumulator, _, i) => {
+      return { ...accumulator, [`${i * 4}`]: `${(i * 4) / 10}rem` };
+    }),
+  };
+};
+
+const PX_ENTRIES = createPxEntries(100);
+
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      spacing: PX_ENTRIES,
+      borderRadius: {
+        none: "0rem",
+        sm: "2rem",
+        md: "3.2rem",
+        lg: "4rem",
+        full: "100rem",
+      },
+      boxShadow: {
+        sm: "0px 12px 40px rgba(0, 0, 0, 0.10)",
+        md: "0px 12px 20px rgba(0, 0, 0, 0.10)",
+        lg: "0px 12px 30px 0px rgba(0, 0, 0, 0.20)",
+      },
+      colors: {
+        black: {
+          0: "var(--black-0)",
+          20: "var(--black-20)",
+          40: "var(--black-40)",
+          60: "var(--black-60)",
+          80: "var(--black-80)",
+          100: "var(--black-100)",
+        },
+        orange: "var(--orange)",
+        sky: "var(--sky)",
+      },
+      fontSize: {
+        12: "1.2rem",
+        14: "1.4rem",
+        16: "1.6rem",
+        18: "1.8rem",
+        20: "2.0rem",
+        24: "2.4rem",
+      },
+      fontWeight: {
+        r: "400",
+        m: "500",
+        b: "600",
       },
     },
   },
