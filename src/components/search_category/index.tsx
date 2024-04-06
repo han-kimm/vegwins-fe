baa614907cafcce7d9e7ea531aaa0305e6d6b672
Haneul-Kim <4188989@naver.com>
2024-04-06 10:54:31 +0900

    feat: 카테고리 자동스크롤

--- src/components/search_category/index.tsx
+++ src/components/search_category/index.tsx
@@ -1,11 +1,11 @@
 'use client';
 
 import { CATEGORY, CATEGORY_KEY } from '@/constants/category';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Icon from '@/components/Icon';
 
 const CategorySelector = () => {
-  const [selected, setSelected] = useState('');
+  const [selected, setSelected] = useState('패스트푸드');
   const toggleSelected = (key: string) => {
     if (selected === key) {
       setSelected('');
@@ -14,11 +14,14 @@ const CategorySelector = () => {
     setSelected(key);
   };
 
+  const autoScrollRef = useAutoScroll();
+
   return (
     <section className="relative flex h-96 gap-12 overflow-scroll scroll-smooth rounded-md bg-white p-12 shadow-md" role="group" aria-label="검색 결과를 필터링하는 카테고리 선택">
       {CATEGORY_KEY.map((key) => (
         <button
           onClick={() => toggleSelected(key)}
+          ref={selected === key ? autoScrollRef : null}
           key={key}
           className={`flex-center w-60 shrink-0 flex-col transition-all duration-500 ${key === selected ? 'text-black-100' : 'text-black-40'}`}
         >
@@ -30,3 +33,18 @@ const CategorySelector = () => {
   );
 };
 export default CategorySelector;
+
+const useAutoScroll = () => {
+  const autoScrollRef = useRef<HTMLButtonElement>();
+  const cbRef = (el: HTMLButtonElement) => {
+    autoScrollRef.current = el;
+  };
+
+  useEffect(() => {
+    if (autoScrollRef.current) {
+      autoScrollRef.current.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+    }
+  }, [autoScrollRef.current]);
+
+  return cbRef;
+};
