685b4a8c8d27eff12b86556e8402dbc5ab2e5788
Haneul-Kim <4188989@naver.com>
2024-04-12 23:06:44 +0900

    feat: LiftingButton에 throttle, debounce 적용

--- src/components/common/LiftingButton.tsx
+++ src/components/common/LiftingButton.tsx
@@ -1,19 +1,31 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import IconArrowUp from 'public/icon/arrow-up.svg';
 
 const LiftingButton = () => {
   const [isScroll, setIsScroll] = useState(false);
+  const throttle = useRef(false);
+  const debounce = useRef<NodeJS.Timeout>();
 
-  const handleScroll = () => {
+  const handleScroll = useCallback(() => {
+    if (throttle.current) {
+      return;
+    }
+    console.log(3);
     const { scrollY } = window;
     scrollY > 200 ? setIsScroll(true) : setIsScroll(false);
-  };
+    throttle.current = true;
+    setTimeout(() => (throttle.current = false), 500);
 
-  const liftScroll = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  };
+    if (debounce.current) {
+      clearTimeout(debounce.current);
+    }
+    debounce.current = setTimeout(() => {
+      const { scrollY } = window;
+      scrollY > 200 ? setIsScroll(true) : setIsScroll(false);
+    }, 500);
+  }, [throttle]);
 
   useEffect(() => {
     handleScroll();
@@ -21,6 +33,10 @@ const LiftingButton = () => {
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
+  }, []);
+
+  const liftScroll = useCallback(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }, []);
 
   return isScroll ? (
