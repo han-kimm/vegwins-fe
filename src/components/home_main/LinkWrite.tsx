0b7a1c1525e57faa5dcb789c517abf8df5377c07
Haneul-Kim <4188989@naver.com>
2024-04-14 17:28:30 +0900

    fix: 간단 수정

--- src/components/home_main/LinkWrite.tsx
+++ src/components/home_main/LinkWrite.tsx
@@ -6,7 +6,7 @@ const LinkWrite = () => {
     <section className="flex w-full gap-12 px-40" aria-label="게시판 또는 문서작성으로 이동">
       <Link href="/write" className="transform-active flex grow justify-between rounded-sm bg-white px-20 py-28 shadow-md">
         <p className="mb-28 text-start text-18 font-bold">문서 작성</p>
-        <Image width={32} height={32} src="/icon/pencil.svg" alt="작성 이미지" aria-hidden={true} />
+        <Image width={32} height={55} src="/icon/pencil.svg" alt="작성 이미지" aria-hidden={true} />
       </Link>
     </section>
   );
--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -3,14 +3,16 @@ import { useCallback, useRef } from 'react';
 const useDebounce = <T>(callback: (...args: T[]) => void, time: number) => {
   const timer = useRef<NodeJS.Timeout>();
 
-  return useCallback((...args: T[]) => {
-    if (timer.current) {
-      clearTimeout(timer.current);
-    }
-    timer.current = setTimeout(() => {
-      console.log('useDebounce', 'set');
-      callback(...args);
-    }, time);
-  }, [timer]);
+  return useCallback(
+    (...args: T[]) => {
+      if (timer.current) {
+        clearTimeout(timer.current);
+      }
+      timer.current = setTimeout(() => {
+        callback(...args);
+      }, time);
+    },
+    [timer],
+  );
 };
 export default useDebounce;
