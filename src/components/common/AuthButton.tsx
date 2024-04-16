13e718d8cc613fa4e2de9370c812269313619c81
Haneul-Kim <4188989@naver.com>
2024-04-16 14:36:13 +0900

    feat: 삼성인터넷은 리다이렉트로 구현

--- src/components/common/AuthButton.tsx
+++ src/components/common/AuthButton.tsx
@@ -10,26 +10,32 @@ declare global {
   }
 }
 
-// const AuthModal = dynamic(() => import('@/components/common/AuthModal'), { ssr: false });
+const AuthModal = dynamic(() => import('@/components/common/AuthModal'), { ssr: false });
 
 const AuthButton = () => {
   const [modalOpen, setModalOpen] = useState(false);
 
   const googleAuth = () => {
     const { google } = window;
-    if ('Identity') {
-      google.accounts.id.initialize({
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    if (userAgent.includes('samsung')) {
+      google.accounts.oauth2.initCodeClient({
         client_id: process.env.NEXT_PUBLIC_ID_GOOGLE,
-        callback: async (response: any) => {
-          // Here we call our Provider with the token provided by google
-          alert('구글 로그인 성공');
-        },
-        itp_support: true,
+        scope: 'https://www.googleapis.com/auth/userinfo.profile',
+        ux_mode: 'popup',
+        redirect_uri: 'http://localhost:8000/auth/google',
       });
-      google.accounts.id.prompt((re: any) => {
-        alert(JSON.stringify(re));
-      });
+      return;
     }
+    google.accounts.id.initialize({
+      client_id: process.env.NEXT_PUBLIC_ID_GOOGLE,
+      callback: async (response: any) => {
+        alert('구글 로그인 성공');
+      },
+      itp_support: true,
+      use_fedcm_for_prompt: true,
+    });
+    google.accounts.id.prompt();
   };
   const googleOneTap = () => {
     const current = document.querySelector('#googleAuth');
@@ -52,7 +58,7 @@ const AuthButton = () => {
         <Image src="/icon/join.svg" alt="로그인 이미지" width={24} height={24} />
         <span>로그인</span>
       </button>
-      {/* {modalOpen && <AuthModal closeModal={() => setModalOpen(false)} />} */}
+      {modalOpen && <AuthModal closeModal={() => setModalOpen(false)} />}
     </>
   );
 };
