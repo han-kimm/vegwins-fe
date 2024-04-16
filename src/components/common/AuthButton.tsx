b80f9db7de4c5c65b9788114f90e889495e87ba6
Haneul-Kim <4188989@naver.com>
2024-04-16 11:34:30 +0900

    feat: 구글원탭 로그인

--- src/components/common/AuthButton.tsx
+++ src/components/common/AuthButton.tsx
@@ -4,17 +4,56 @@ import dynamic from 'next/dynamic';
 import Image from 'next/image';
 import { useState } from 'react';
 
-const AuthModal = dynamic(() => import('@/components/common/AuthModal'), { ssr: false });
+declare global {
+  interface Window {
+    google: any;
+  }
+}
+
+// const AuthModal = dynamic(() => import('@/components/common/AuthModal'), { ssr: false });
 
 const AuthButton = () => {
   const [modalOpen, setModalOpen] = useState(false);
+
+  const googleAuth = () => {
+    const { google } = window;
+    if (google) {
+      google.accounts.id.initialize({
+        client_id: process.env.NEXT_PUBLIC_ID_GOOGLE,
+        callback: async (response: any) => {
+          // Here we call our Provider with the token provided by google
+          alert('구글 로그인 성공');
+        },
+        use_fedcm_for_prompt: true,
+        itp_support: true,
+        auto_select: true,
+      });
+    }
+
+    google.accounts.id.prompt();
+  };
+  const googleOneTap = () => {
+    const current = document.querySelector('#googleAuth');
+    if (current) {
+      googleAuth();
+      return;
+    }
+    const script = document.createElement('script');
+    script.id = 'googleAuth';
+    script.src = 'https://accounts.google.com/gsi/client';
+    script.async = true;
+    script.onload = googleAuth;
+
+    document.head.append(script);
+  };
+
   return (
     <>
-      <button onClick={() => setModalOpen(true)} className="flex-center gap-8 text-16 font-medium">
+      <button onClick={googleOneTap} className="flex-center gap-8 text-16 font-medium">
         <Image src="/icon/join.svg" alt="로그인 이미지" width={24} height={24} />
         <span>로그인</span>
       </button>
-      {modalOpen && <AuthModal closeModal={() => setModalOpen(false)} />}
+      {/* {modalOpen && <AuthModal closeModal={() => setModalOpen(false)} />} */}
     </>
   );
 };
