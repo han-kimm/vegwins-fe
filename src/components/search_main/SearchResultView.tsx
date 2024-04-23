d7793359384eb68b269aca8c648fdcf964bd32ff
han-kimm <4188989@naver.com>
2024-04-24 00:19:11 +0900

    feat: 검색 레이블 수정

--- src/components/search_main/SearchResultView.tsx
+++ src/components/search_main/SearchResultView.tsx
@@ -16,21 +16,10 @@ const SearchResultView = async ({ c, k }: Props) => {
     cache: 'no-cache',
   });
 
-  const setLabel = () => {
-    if (c && k) {
-      return `${c}: "${k}" 검색 결과(${data.length})`;
-    } else if (c) {
-      return `${c}: 검색 결과(${data.length})`;
-    } else if (k) {
-      return `"${k}" 검색 결과(${data.length})`;
-    } else if (data.length) {
-      return '최근 작성된 문서';
-    }
-  };
   return (
     <section className="flex flex-grow flex-col" aria-label="검색 목록">
       <div className="mb-12 ml-20 flex items-center">
-        <h1 className="text-18 font-bold">{setLabel()}</h1>
+        <h1 className="text-18 font-bold">{setLabel(data, c, k)}</h1>
         {(!!c || !!k) && <SearchResultReset />}
       </div>
       {data.length ? (
@@ -49,3 +38,18 @@ const SearchResultView = async ({ c, k }: Props) => {
   );
 };
 export default SearchResultView;
+
+const setLabel = (data: SearchItem[], c?: string, k?: string) => {
+  if (!data.length) {
+    return null;
+  }
+  if (c && k) {
+    return `${c}: "${k}" 검색 결과(${data.length})`;
+  } else if (c) {
+    return `${c}: 검색 결과(${data.length})`;
+  } else if (k) {
+    return `"${k}" 검색 결과(${data.length})`;
+  } else if (data.length) {
+    return '최근 작성된 문서';
+  }
+};
