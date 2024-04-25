049794aa62918e57d68c8b94577baf0a8963ee52
han-kimm <4188989@naver.com>
2024-04-25 21:58:00 +0900

    fix: 바텀시트 동작 변경

--- src/components/home_header/AuthCheckerAfter.tsx
+++ src/components/home_header/AuthCheckerAfter.tsx
@@ -1,7 +1,8 @@
 import dynamic from 'next/dynamic';
-import Dashboard from '@/components/dashboard/Dashboard';
 import DashboardBottomSheet from '@/components/home_header/DashboardBottomSheet';
 import Notice from '@/components/home_header/Notice';
+
+const Dashboard = dynamic(() => import('@/components/dashboard/Dashboard'));
 
 const AuthAfter = () => {
   return (
--- src/components/home_header/DashboardBottomSheet.tsx
+++ src/components/home_header/DashboardBottomSheet.tsx
@@ -8,40 +8,33 @@ interface Props {
 }
 const DashboardBottomSheet = ({ children }: Props) => {
   const [open, setOpen] = useState(false);
-  const [focus, setFocus] = useState(false);
 
   const closeByBlur = () => {
     setOpen(false);
   };
 
-  const buttonClick = () => {
-    if (focus) {
-      setOpen(false);
-      setFocus(false);
-      return;
-    }
+  const openByButton = () => {
     setOpen((prev) => !prev);
-  };
-
-  const sheetFocus = () => {
-    setOpen(true);
-    setFocus(true);
   };
 
   return (
     <>
-      <button onClick={buttonClick} onBlur={closeByBlur} aria-label="내 정보 모아보기">
+      <button onClick={openByButton} aria-label="내 정보 모아보기">
         <IconProfile />
       </button>
-      <div
-        tabIndex={0}
-        id="bottomsheet"
-        onFocus={sheetFocus}
-        onBlur={closeByBlur}
-        className={`${open ? 'bottom-0' : '-bottom-300'} fixed left-0 z-20 flex h-max w-full max-w-[50rem] animate-slideDown flex-col rounded-t-md bg-white shadow-bt transition-all duration-300 max:mx-[calc(50%-25rem)]`}
-      >
-        {children}
-      </div>
+      {open && (
+        <div
+          id="bottomsheet"
+          tabIndex={0}
+          ref={(el) => {
+            el?.focus();
+          }}
+          onBlur={closeByBlur}
+          className={`${open ? 'bottom-0' : '-bottom-300'} fixed left-0 z-20 flex h-max w-full max-w-[50rem] animate-slideDown flex-col rounded-t-md bg-white shadow-bt transition-all duration-300 max:mx-[calc(50%-25rem)]`}
+        >
+          {children}
+        </div>
+      )}
     </>
   );
 };
