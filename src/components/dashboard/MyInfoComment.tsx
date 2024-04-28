3819a9231c61d116bd40fbfcc61f8f93bbf7d775
han-kimm <4188989@naver.com>
2024-04-28 21:51:41 +0900

    refactor: 대시보드 댓글에서도 구조 변경

--- src/components/dashboard/MyInfoComment.tsx
+++ src/components/dashboard/MyInfoComment.tsx
@@ -1,6 +1,7 @@
 import useFetchedState from '@/hooks/useFetchedState';
 import { Comment } from '@/types/data';
 import { Session } from '@/types/session';
+import { Fragment } from 'react';
 import DeferredSpinner from '@/components/errorHandling/DeferredSpinner';
 import UsersCommentItem from '@/components/paper_main/UsersCommentItem';
 
@@ -16,7 +17,12 @@ const MyInfoComment = ({ session }: Props) => {
   return (
     <div className="scrollbar flex max-h-400 min-h-108 flex-col gap-12 overflow-y-scroll pr-20">
       {myComment.length ? (
-        myComment.map((comment) => <UsersCommentItem key={comment._id} session={session} comment={comment} my setMyComment={setState} />)
+        myComment.map((comment) => (
+          <Fragment key={comment._id}>
+            <UsersCommentItem session={session} comment={comment} setMyComment={setState} />
+            <hr className="border-black-60" />
+          </Fragment>
+        ))
       ) : (
         <p className="m-auto text-16">작성한 댓글이 없습니다.</p>
       )}
