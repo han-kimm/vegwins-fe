7480b067c5a503c2107c012765d683951a685cda
han-kimm <4188989@naver.com>
2024-04-29 21:32:27 +0900

    feat: 나의 댓글 수정기능 추가

--- src/components/dashboard/MyInfoComment.tsx
+++ src/components/dashboard/MyInfoComment.tsx
@@ -1,3 +1,4 @@
+import useEditComment from '@/hooks/useEditComment';
 import useFetchedState from '@/hooks/useFetchedState';
 import { Comment } from '@/types/data';
 import { Session } from '@/types/session';
@@ -10,6 +11,7 @@ interface Props {
 }
 const MyInfoComment = ({ session }: Props) => {
   const { state: myComment, setState, pending } = useFetchedState<Comment[]>({ init: [], path: '/user/comment', deps: [] });
+  const { targetComment, resetTarget, setRecomment, ButtonEdit } = useEditComment();
 
   if (pending) {
     return <DeferredSpinner />;
@@ -17,12 +19,22 @@ const MyInfoComment = ({ session }: Props) => {
   return (
     <div className="scrollbar flex max-h-400 min-h-108 flex-col gap-12 overflow-y-scroll pr-20">
       {myComment.length ? (
-        myComment.map((comment) => (
-          <Fragment key={comment._id}>
-            <UsersCommentItem session={session} comment={comment} onSuccess={() => setState((prev) => prev.filter((c) => c._id !== comment._id))} />
-            <hr className="border-black-60" />
-          </Fragment>
-        ))
+        myComment.map((comment) => {
+          const isEdited = targetComment?.status === 'edit' && targetComment.comment._id === comment._id;
+
+          return (
+            <Fragment key={comment._id}>
+              <UsersCommentItem
+                session={session}
+                comment={comment}
+                onSuccess={() => setState((prev) => prev.filter((c) => c._id !== comment._id))}
+                isEdited={isEdited}
+                ButtonEdit={ButtonEdit}
+              />
+              <hr className="border-black-60" />
+            </Fragment>
+          );
+        })
       ) : (
         <p className="m-auto text-16">작성한 댓글이 없습니다.</p>
       )}
