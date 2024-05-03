8b0f2937832cc0263896efd6950916583929e239
han-kimm <4188989@naver.com>
2024-05-03 18:51:23 +0900

    test: 구글 리다이렉트

--- src/app/auth/google/page.tsx
+++ src/app/auth/google/page.tsx
@@ -20,7 +20,7 @@ const GoogleAuthPage = ({ searchParams }: Props) => {
     (async () => {
       try {
         const { code } = searchParams;
-        console.log(searchParams);
+        console.log(code);
         const { accessToken, refreshToken, nickname } = await fetch(process.env.NEXT_PUBLIC_BASE_URL + '/auth/google', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
