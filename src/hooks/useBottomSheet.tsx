1899dcffacccfd86b8adc0d541a7c4418e6be342
han-kimm <4188989@naver.com>
2024-05-04 01:02:06 +0900

    test

--- src/hooks/useBottomSheet.tsx
+++ src/hooks/useBottomSheet.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FunctionComponent, MouseEvent, ReactNode, useCallback, useState } from 'react';
+import { FunctionComponent, MouseEvent, ReactNode, TouchEvent, useCallback, useState } from 'react';
 import IconDown from 'public/icon/arrow-down.svg';
 
 const useBottomSheet = () => {
@@ -19,7 +19,11 @@ const useBottomSheet = () => {
           onScroll={() => console.log(1)}
           className={`${open ? 'bottom-0' : '-bottom-300'} fixed left-0 z-20 flex h-dvh w-full max-w-[50rem] animate-slideDown flex-col justify-end max:mx-[calc(50%-25rem)]`}
         >
-          <div onClick={(e: MouseEvent) => e.stopPropagation()} className="rounded-t-md bg-white shadow-bt">
+          <div
+            onClick={(e: MouseEvent) => e.stopPropagation()}
+            onTouchEnd={(e: TouchEvent) => e.stopPropagation()}
+            className="rounded-t-md bg-white shadow-bt"
+          >
             <div className="flex justify-between px-16 pt-16">
               <div className="h-4 w-100 rounded-full bg-black-100" />
               <button onClick={toggleByClick} className="text-b" aria-label="접어두기">
