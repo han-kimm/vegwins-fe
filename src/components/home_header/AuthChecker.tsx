86dd5fca0ec73d7dacfc9b1023c9604bcf33b3de
han-kimm <4188989@naver.com>
2024-05-14 23:22:07 +0900

    fix: manifest 삭제

--- src/components/home_header/AuthChecker.tsx
+++
@@ -1,9 +0,0 @@
-import { getCookie } from '@/utils/cookie';
-import AuthCheckerButton from '@/components/common/AuthButton';
-import AuthCheckerAfter from '@/components/home_header/AuthCheckerAfter';
-
-const AuthChecker = async () => {
-  const session = await getCookie('v_s');
-  return session ? <AuthCheckerAfter /> : <AuthCheckerButton />;
-};
-export default AuthChecker;
--- src/types/session.ts
+++
@@ -1,3 +0,0 @@
-export type Session = {
-  nickname: string;
-} | null;
